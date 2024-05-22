b8da00a8f5bcde5585974059e5a6b9f1293e101a
mumbawa <fgomezc77@gmail.com>
2024-05-22 07:15:01 -0700

    Inconspicuous change in PolyanetsEndpoints.py

--- api_client/PolyanetsEndpoints.py
+++ api_client/PolyanetsEndpoints.py
@@ -1,13 +1,13 @@
 class PolyanetsEndpoints:
-    def __init__(self, client):
-        self.client = client
+    def __init__(self, api_client):
+        self.api_client = api_client
 
     def add_polyanet(self, row, column):
         payload = {'row': row, 'column': column}
-        return self.client.post('polyanets', payload)
+        return self.api_client.post('polyanets', payload)
 
     def delete_polyanet(self, row, column):
         payload = {'row': row, 'column': column}
-        return self.client.delete('polyanets', payload)
+        return self.api_client.delete('polyanets', payload)
 
 
